8ea6e0e975ee63d48df4a744b75679ce89e4bb0b
AntoineEsbelin <74203837+AntoineEsbelin@users.noreply.github.com>
2023-10-03 19:40:02 +0200

    Achievement

--- Assets/Script/Team NyaNya/Observer/AchievementManager.cs
+++ Assets/Script/Team NyaNya/Observer/AchievementManager.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Assertions.Must;
+using NaughtyAttributes;
 
-
-    public class AchievementManager : MonoBehaviour
+public class AchievementManager : MonoBehaviour
     {
-        public static List<Achievement> achievements;
+    public static List<Achievement> achievements;
 
-        public int achievementCount = 0;
+    [SerializeField] int achievementTotalCount = 0;
+    public static int achievementsDone = 0;
 
-        // Juste pour tester
-        public int intTest;
-       // public GameObject gameObjectTest;
-
-        public bool AchievementUnlocked(string achievementName)
+    // Juste pour tester
+    [SerializeField] int intTest;
+    [SerializeField] GameObject gameObjectTest;
+    //---------------
+    [Header("Paramètres de l'achievement")]
+    [SerializeField] bool showParameters;
+    [SerializeField, ShowIf("showParameters")] string nameTheAchievement;
+    [SerializeField, ShowIf("showParameters")] string describeTheAchievement;
+    [SerializeField, ShowIf("showParameters")] int prerequisiteIntToAchieve = 2;
+    [Button]
+    public void AddAchievement()
+    {
+        achievements.Add(new Achievement(nameTheAchievement, describeTheAchievement, (object o) => intTest >= prerequisiteIntToAchieve ));
+        achievementTotalCount++;
+    }
+    [Button]
+    public void AchievementDone()
+    {
+        Debug.Log(achievementsDone);
+    }
+    public bool AchievementUnlocked(string achievementName)
         {
             bool result = false;
 
@@ -23,12 +40,12 @@ using UnityEngine.Assertions.Must;
                 return false;
 
             Achievement[] achievementsArray = achievements.ToArray();
-            Achievement a = Array.Find(achievementsArray, ach => achievementName == ach.name);
+            Achievement a = Array.Find(achievementsArray, ach => achievementName == ach.Name);
 
             if(a == null)
                 return false;
 
-            result = a.achieved;
+            result = a.Achieved;
 
             return result;
         }
@@ -45,11 +62,11 @@ using UnityEngine.Assertions.Must;
 
             achievements = new List<Achievement>();
             achievements.Add(new Achievement("Integer", "le nombre doit être supérieur ou égal à 5", (object o) => intTest >= 5));    
-          //  achievements.Add(new Achievement("La bonne couleur !", "La couleur de l'objet doit être rouge", (object o) => gameObjectTest.GetComponent<SpriteRenderer>().color.Equals(Color.blue)));
+            achievements.Add(new Achievement("La bonne couleur !", "La couleur de l'objet doit être bleu foncé", (object o) => gameObjectTest.GetComponent<SpriteRenderer>().color.Equals(Color.blue)));
 
             for(int i = 0; i < achievements.Count; i++) 
             {
-                achievementCount++;
+                achievementTotalCount++;
             }
         }
 
@@ -62,16 +79,16 @@ using UnityEngine.Assertions.Must;
         private void CheckCompletion()
         {
             if (achievements == null)
-                return;
+               return;
+        
 
-            foreach(var achievement in achievements)
+        foreach (var achievement in achievements)
             {
-                achievement.UpdateCompletion();
+                achievement.Completion();              
             }
         }
     }
-
-    public class Achievement
+public class Achievement
     {
         public Achievement(string name, string description, Predicate<object> required)
         {
@@ -81,21 +98,25 @@ using UnityEngine.Assertions.Must;
        
         }
 
-        public string name;
-        public string description;
-        public Predicate<object> required;
+    [SerializeField] string name;
+    [SerializeField] string description;
+    [SerializeField] Predicate<object> required;
 
-        public bool achieved;
+    [SerializeField] bool achieved;
 
-        public void UpdateCompletion()
+    public string Name { get => name; }
+    public bool Achieved { get => achieved; }
+
+    public void Completion()
         {
             if (achieved)
                 return;
             if (RequirementsMets())
             {
-                Debug.Log($"{name}: {description}");
+                Debug.Log(name + " : " + description);
                 achieved = true;
-            }
+                AchievementManager.achievementsDone++;               
+             }
         }
 
         public bool RequirementsMets()
